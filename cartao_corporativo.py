e2c309f647ad46065470ec6130585d71e824707e
Tayling <taymng@yahoo.com.br>
2024-01-30 00:20:55 -0300

    Atualização do README

--- cartao_corporativo.py
+++ cartao_corporativo.py
@@ -1,3 +1,5 @@
 import pandas as pd
 df = pd.read_csv(r'C:\Users\Tayling\OneDrive\Dados\Gastos Governo_Datasets\cartao-corporativo\dados_brutos\Planilha1_2003a2022.csv',sep=';', encoding = 'iso-8859-1')
 print(df.dtypes)
+
+
